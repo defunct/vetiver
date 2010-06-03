b8c2f52960cf3d274dcd4262b85a55120c921bd2
Alan Gutierrez <alan@blogometer.com>
2010-06-03 02:48:42 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/vetiver/mix/VetiverProject.java
+++ src/mix/java/com/goodworkalan/vetiver/mix/VetiverProject.java
@@ -4,16 +4,24 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class VetiverProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Spawn.
+ *
+ * @author Alan Gutierrez
+ */
+public class VetiverProject implements ProjectModule {
+    /**
+     * Build the project definition for Spawn.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.vetiver/vetiver/0.1")
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .depends()
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();
